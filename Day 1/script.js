60edca55ab5bc6b9dc7df53c0369cc268b103952
Vikram Jangid <56400249+VikramJangid-dev@users.noreply.github.com>
2023-10-07 15:11:13 +0530

    Add files via upload

---
+++ Day 1/script.js
@@ -0,0 +1,28 @@
+// Open console using inspect
+
+// displaying messages on console using console.log()
+console.log("Hey, Started Js");
+
+// Mujhe ek Popup message chaiye website pe ?
+
+// alert() --> work as ouput
+
+alert("hey, I'm learning js and This is Day-1");
+
+// Prompt --> work as input
+
+prompt("Kya haal chal hain ?");
+
+// console ka bhaii --> log , warn , error
+
+console.log("Its is log");
+
+console.warn("Its is warning");
+
+console.error("Its is error");
+
+// Mini Projects
+
+var name = prompt("what is your Name?");
+console.log("Welcome " + name + "to the js journey");
+alert("Welcome " + name + " to the js journey");
